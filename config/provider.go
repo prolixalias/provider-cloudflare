0db1de30db252500f8328ac95959a506c42aeca0
Paul Talbot <prolixalias@gmail.com>
2026-01-13 14:14:34 -0500

    initial commit

---
+++ config/provider.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	// Note(turkenh): we are importing this to embed provider schema document
+	_ "embed"
+
+	ujconfig "github.com/crossplane/upjet/v2/pkg/config"
+
+	// Resource group configurations will be added here
+	// e.g., "github.com/prolixalias/provider-cloudflare/config/zone"
+)
+
+const (
+	resourcePrefix = "cloudflare"
+	modulePath     = "github.com/prolixalias/provider-cloudflare"
+)
+
+//go:embed schema.json
+var providerSchema string
+
+//go:embed provider-metadata.yaml
+var providerMetadata string
+
+// GetProvider returns provider configuration
+func GetProvider() *ujconfig.Provider {
+	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
+		ujconfig.WithRootGroup("cloudflare.upbound.io"),
+		ujconfig.WithShortName("cloudflare"),
+		ujconfig.WithIncludeList(ExternalNameConfigured()),
+		ujconfig.WithFeaturesPackage("internal/features"),
+		ujconfig.WithDefaultResourceOptions(
+			ExternalNameConfigurations(),
+		))
+
+	for _, configure := range []func(provider *ujconfig.Provider){
+		// Resource group Configure functions will be added here
+		// e.g., zone.Configure, dns.Configure, worker.Configure
+	} {
+		configure(pc)
+	}
+
+	pc.ConfigureResources()
+	return pc
+}
+
+// GetProviderNamespaced returns the namespaced provider configuration
+func GetProviderNamespaced() *ujconfig.Provider {
+	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
+		ujconfig.WithRootGroup("cloudflare.m.upbound.io"),
+		ujconfig.WithShortName("cloudflare"),
+		ujconfig.WithIncludeList(ExternalNameConfigured()),
+		ujconfig.WithFeaturesPackage("internal/features"),
+		ujconfig.WithDefaultResourceOptions(
+			ExternalNameConfigurations(),
+		),
+		ujconfig.WithExampleManifestConfiguration(ujconfig.ExampleManifestConfiguration{
+			ManagedResourceNamespace: "crossplane-system",
+		}))
+
+	for _, configure := range []func(provider *ujconfig.Provider){
+		// Resource group Configure functions will be added here
+	} {
+		configure(pc)
+	}
+
+	pc.ConfigureResources()
+	return pc
+}
