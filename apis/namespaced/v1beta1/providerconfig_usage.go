1a95adde5ccf83c7c5b6a3bf681d2a2fb73e110b
Paul Talbot <prolixalias@gmail.com>
2026-01-26 16:51:01 -0500

    wip

--- apis/namespaced/v1beta1/providerconfig_usage.go
+++ apis/namespaced/v1beta1/providerconfig_usage.go
@@ -4,7 +4,11 @@
 
 package v1beta1
 
-import xpv1 "github.com/crossplane/crossplane-runtime/v2/apis/common/v1"
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+
+	xpv1 "github.com/crossplane/crossplane-runtime/v2/apis/common/v1"
+)
 
 // GetProviderConfigReference of this ProviderConfigUsage.
 func (p *ProviderConfigUsage) GetProviderConfigReference() xpv1.ProviderConfigReference {
@@ -24,4 +28,13 @@ func (p *ProviderConfigUsage) SetProviderConfigReference(r xpv1.ProviderConfigRe
 // SetResourceReference of this ProviderConfigUsage.
 func (p *ProviderConfigUsage) SetResourceReference(r xpv1.TypedReference) {
 	p.ResourceReference = r
+}
+
+// GetItems returns the list of ProviderConfigUsage as runtime.Object
+func (l *ProviderConfigUsageList) GetItems() []runtime.Object {
+	res := make([]runtime.Object, len(l.Items))
+	for i := range l.Items {
+		res[i] = &l.Items[i]
+	}
+	return res
 }
